2dd1239ddfe203d88cf2d96f30b18c8e6d89f745
William <hello@bigwilliam.com>
2013-10-24 11:51:40 -0700

    Update wp-config.php
    added link to the official WP documentation for editing wp_config.php

--- base-files/wp-config.php
+++ base-files/wp-config.php
@@ -42,9 +42,10 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/******************
- * Other settings *
- ******************/
+/***************************************************************
+ * Other settings                                              *
+ * More here: http://codex.wordpress.org/Editing_wp-config.php *
+ ***************************************************************/
 
 /* Memory Limit */
 //define( 'WP_MEMORY_LIMIT', '64M' );

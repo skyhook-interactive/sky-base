55fcc2dea5eb2302a3c85809ce61a5f22e212bc3
William Cobb <hello@bigwilliam.com>
2013-11-26 14:02:25 -0700

    added config file

---
+++ config.sh
@@ -0,0 +1,10 @@
+### CONFIGURATION ###
+# Set your config settings here
+
+LOCAL_MYSQL_USER = ''
+LOCAL_MYSQL_PASS = ''
+LOCAL_MYSQL_HOST = '127.0.0.1'
+
+HOSTS_FILE_LOC    = '/etc/hosts'
+APACHE_VHOSTS_LOC = '/Applications/XAMPP/xamppfiles/etc/extra/httpd-vhosts.conf'
+

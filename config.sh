e020a47a16bee8161efb165a1b4ba1f7367a444e
William <hello@bigwilliam.com>
2013-12-17 14:35:28 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,5 +6,5 @@ LOCAL_MYSQL_PASS = ''
 LOCAL_MYSQL_HOST = '127.0.0.1'
 
 HOSTS_FILE_LOC    = '/etc/hosts'
-APACHE_VHOSTS_LOC = '/Applications/XAMPP/xamppfiles/etc/extra/httpd-vhosts.conf'
+APACHE_VHOSTS_LOC = '/etc/apache2/extra/httpd-vhosts.conf'
 
